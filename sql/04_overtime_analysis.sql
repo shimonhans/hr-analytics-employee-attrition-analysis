3742cc79779316a5cb594033f90661b35ba9b770
shimonhans <shimonsimanjuntak81@gmail.com>
2026-03-23 11:05:14 +0700

    Create 04_overtime_analysis.sql

---
+++ sql/04_overtime_analysis.sql
@@ -0,0 +1,14 @@
+SELECT 
+  OverTime,
+  COUNT(*) AS total_employees,
+  COUNTIF(Attrition = TRUE) AS employees_left,
+  ROUND(COUNTIF(Attrition = TRUE) / COUNT(*) * 100, 2) AS attrition_rate
+FROM `dataset_attrition_IBM.cleaned_attrition_data`
+GROUP BY OverTime
+ORDER BY attrition_rate DESC
+
+      /*
+      Row	OverTime	total_employees	employees_left	attrition_rate
+      1	  true	    416	            127	            30.53
+      2	  false	    1054	          110	            10.44
+      */
