ab4abee9f3c37aec4ccb2d579b5bbf7f3dca3df2
shimonhans <shimonsimanjuntak81@gmail.com>
2026-03-23 11:04:02 +0700

    Create 02_attrition_rate.sql

---
+++ sql/02_attrition_rate.sql
@@ -0,0 +1,8 @@
+SELECT 
+  COUNT(*) AS total_employees,
+  COUNTIF(Attrition = TRUE) AS employees_left,
+  ROUND(COUNTIF(Attrition = TRUE) / COUNT(*) * 100, 2) AS attrition_rate_percent
+FROM `dataset_attrition_IBM.cleaned_attrition_data`
+
+
+    # COMPANY ATTRITION RATE --> 16.12%
